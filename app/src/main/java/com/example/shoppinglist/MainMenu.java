c4e9986106755d0fe80d8d850552ed146cbeddc6
KBako <kieranzkbako@gmail.com>
2022-03-15 15:20:22 -0700

    Working Fragment navigation

--- app/src/main/java/com/example/shoppinglist/MainMenu.java
+++ app/src/main/java/com/example/shoppinglist/MainMenu.java
@@ -2,11 +2,17 @@ package com.example.shoppinglist;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.shoppinglist.databinding.FragmentFirstBinding;
+import com.example.shoppinglist.databinding.FragmentMainMenuBinding;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,33 +21,16 @@ import android.view.ViewGroup;
  */
 public class MainMenu extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentMainMenuBinding binding;
 
     public MainMenu() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainMenu.
-     */
-    // TODO: Rename and change types and number of parameters
+
     public static MainMenu newInstance(String param1, String param2) {
         MainMenu fragment = new MainMenu();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +38,40 @@ public class MainMenu extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        //return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        binding = FragmentMainMenuBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MainMenu.this)
+                        .navigate(R.id.action_mainMenu_to_FirstFragment);
+            }
+        });
+
+        binding.secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(MainMenu.this)
+                        .navigate(R.id.action_mainMenu_to_SecondFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
